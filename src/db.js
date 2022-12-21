8ddfc5a7b5c2a1b5ee39d87f5bc33cc6597fdb10
Mauri-salazar <maurisa2004@gmail.com>
2022-12-21 14:49:51 -0300

    config docker

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  POSTGRES_USER, POSTGRES_PASSWORD,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const sequelize = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@5432/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
